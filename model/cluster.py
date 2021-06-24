4aa875b6ef05e383b72dc03d1a0769ae2f94d870
aI-area <59239422+aI-area@users.noreply.github.com>
2021-06-24 09:23:16 +0800

    Update cluster.py

--- model/cluster.py
+++ model/cluster.py
@@ -2,7 +2,6 @@ import numpy as np
 import scipy.sparse as sp
 from sklearn.cluster import KMeans
 from evaluator.metrics import clustering_metrics
-from evaluator.dist_modularity import cal_modularity
 
 
 def commit_k_mean(embedding, origin_feature, adj_sp, true_label, cluster_k, params):
@@ -22,7 +21,6 @@ def commit_k_mean(embedding, origin_feature, adj_sp, true_label, cluster_k, para
     ac = np.zeros(rep)
     nm = np.zeros(rep)
     f1 = np.zeros(rep)
-    dm = np.zeros(rep)
 
     u, _, _ = sp.linalg.svds(embedding, k=cluster_k, which='LM')
 
@@ -32,12 +30,10 @@ def commit_k_mean(embedding, origin_feature, adj_sp, true_label, cluster_k, para
         predict_labels = kmeans.predict(u)
         cm = clustering_metrics(true_label, predict_labels)
         ac[i], nm[i], f1[i] = cm.evaluationClusterModelFromLabel()
-        dm[i] = cal_modularity(adj_sp, predict_labels, origin_feature, epsilon_=params['epsilon'])
 
     acc_means = np.mean(ac) * 100
     nmi_means = np.mean(nm) * 100
     f1_means = np.mean(f1) * 100
-    dm_means = np.mean(dm) * 100
 
     msg = '|acc_mean:%.4f|nmi_mean:%.4f|f1_mean:%.4f|' \
           % (acc_means, nmi_means, f1_means)
